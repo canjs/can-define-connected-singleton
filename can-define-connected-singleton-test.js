291be698a0836cb4ea188f2e8cdc4eaae2873562
Nils Lundquist <nils@bitovi.com>
2019-06-03 17:17:40 -0600

    renamed project, removed dependency on can/map events, updated docs

--- can-define-connected-singleton-test.js
+++ can-define-connected-singleton-test.js
@@ -2,9 +2,9 @@
 
 var QUnit = require('steal-qunit');
 var DefineMap = require('can-define/map/map');
-var singleton = require('./can-define-singleton');
+var singleton = require('./can-define-connected-singleton');
 
-QUnit.module('can-define-singleton');
+QUnit.module('can-define-connected-singleton');
 
 QUnit.test('Works as a simple class @decorator', function(){
 	var MyType = DefineMap.extend({});
@@ -27,7 +27,7 @@ QUnit.test('Works as a @decorator({ with_options })', function(){
 
 QUnit.test('Allows configurable property name', function(){
 	var MyType = DefineMap.extend({});
-	var Decorated = singleton({ propertyName: 'foo' })(MyType);
+	var Decorated = singleton({ currentPropertyName: 'foo' })(MyType);
 
 	QUnit.ok(Decorated.hasOwnProperty('foo'));
 	QUnit.ok(Decorated.hasOwnProperty('fooPromise'));
@@ -65,7 +65,7 @@ QUnit.test('Allows for configurable data method name', function(assert){
 		}
 	}, {});
 
-	var Decorated = singleton({ dataMethodName: 'doFooBar' })(MyType);
+	var Decorated = singleton({ fetchMethodName: 'doFooBar' })(MyType);
 
 	QUnit.equal(Decorated.current, undefined, 'initially undefined');
 	Decorated.currentPromise.then(function() {
@@ -74,26 +74,34 @@ QUnit.test('Allows for configurable data method name', function(assert){
 	});
 });
 
-QUnit.test('Creating/saving a new instance updates the "current" property', function(assert){
-	assert.expect(3);
+// TODO: test configurable saving param, saving method & destroy method
+
+QUnit.test('Saving a new instance updates the "current" property', function(assert){
+	assert.expect(4);
 	var done = assert.async();
 	var MyType = singleton(
 		DefineMap.extend({ 
 			get: function() {
-				return Promise.resolve();
+				assert.ok(true, 'Get called but returns undefined. Should only happen once during this test.');
+				return Promise.resolve(undefined);
+			},
+		}, {
+			save: function() {
+				return Promise.resolve(this);
 			}
-		}, {})
+		})
 	);
 
 	var instance = new MyType();
 	QUnit.notEqual(MyType.current, instance);
-	
-	MyType.dispatch('created', [instance]);
-	QUnit.equal(MyType.current, instance);
 
-	MyType.currentPromise.then(function(value) {
-		QUnit.equal(value, instance);
-		done();
+	instance.save().then(() => {
+		QUnit.equal(MyType.current, instance);
+
+		MyType.currentPromise.then(function(value) {
+			QUnit.equal(value, instance);
+			done();
+		});
 	});
 });
 
@@ -105,20 +113,29 @@ QUnit.test('Destroying sets the "current" property to undefined, with rejected p
 			get: function() {
 				return Promise.resolve();
 			}
-		}, {})
+		}, {
+			save: function() {
+				return Promise.resolve(this);
+			},
+			destroy: function() {
+				return Promise.resolve();
+			}
+		})
 	);
 
 	var instance = new MyType();
 	
-	MyType.dispatch('created', [instance]);
-	MyType.dispatch('destroyed', [instance]);
-	QUnit.equal(MyType.current, undefined);
+	instance.save().then(() => {
+		instance.destroy().then(() => {
+			QUnit.equal(MyType.current, undefined);
 
-	MyType.currentPromise.then(function() {
-		QUnit.notOk(true, 'should not get here');
-		done();
-	}).catch(function(value) {
-		QUnit.equal(value, undefined);
-		done();
+			MyType.currentPromise.then(function() {
+				QUnit.notOk(true, 'should not get here');
+				done();
+			}).catch(function(value) {
+				QUnit.equal(value, 'Singleton instance has been destroyed. Save a new instance to reinitialize the singleton.');
+				done();
+			});
+		});
 	});
 });
---
+++ can-define-connected-singleton.js
@@ -0,0 +1,166 @@
+'use strict';
+
+
+var DefineMap = require('can-define/map/map');
+var ObservationRecorder = require('can-observation-recorder');
+var zoneStorage = require('./util/zone-storage');
+var helpers = require('./util/helpers');
+
+var defaults = {
+	storagePrefix: 'can-define-connected-singleton',
+	currentPropertyName: 'current',
+	savingPropertyName: 'saving',
+	fetchMethodName: 'get',
+	createMethodName: 'save',
+	destroyMethodName: 'destroy',
+};
+
+function isDefineMapConstructor(Obj) {
+	return Obj && (Obj.prototype instanceof DefineMap);
+}
+
+// wrap the createMethod so it updates .current, .currentPromise & .saving during the course of a singleton model being persisted
+function wrapCreateMethod(Ctor, options) {
+	const baseCreate = Ctor.prototype[options.createMethodName];
+
+	Ctor.prototype[options.createMethodName] = function wrappedCreate() {
+		var ret = baseCreate.apply(this, arguments);
+
+		// set Ctor.saving & Ctor.currentPromise
+		zoneStorage.setItem(options.storageKeys.savingProperty, this);
+		Ctor.dispatch(options.savingPropertyName, [this]);
+		zoneStorage.setItem(options.storageKeys.currentPropertyPromise, ret);
+		Ctor.dispatch(options.currentPropertyPromiseName, [ret]);
+
+		ret.then((instance) => {
+			// clear Ctor.saving, set Ctor.current
+			zoneStorage.setItem(options.storageKeys.savingProperty, undefined);
+			Ctor.dispatch(options.savingPropertyName, [undefined]);
+			zoneStorage.setItem(options.storageKeys.currentProperty, instance);
+			Ctor.dispatch(options.savingPropertyName, [instance]);
+		}).catch(() => {
+			// clear saving
+			zoneStorage.setItem(options.storageKeys.savingProperty, undefined);
+			Ctor.dispatch(options.savingPropertyName, [undefined]);
+		});
+
+		return ret;
+	};
+}
+
+// wrap the destroyMethod so it updates .current & .currentPromise during the course of a singleton model being removed from persistence
+function wrapDestroyMethod(Ctor, options) {
+	const baseDestroy = Ctor.prototype[options.destroyMethodName];
+
+	Ctor.prototype[options.destroyMethodName] = function wrappedDestroy() {
+		var ret = baseDestroy.apply(this, arguments);
+
+		ret.then(() => {
+			var promise = Promise.reject('Singleton instance has been destroyed. Save a new instance to reinitialize the singleton.');
+
+			// clear current, reject currentPromise w/ reason string if successful
+			zoneStorage.setItem(options.storageKeys.currentProperty, undefined);
+			Ctor.dispatch(options.currentPropertyName, [undefined]);
+			zoneStorage.setItem(options.storageKeys.currentPropertyPromise, promise);
+			Ctor.dispatch(options.currentPropertyPromiseName, [promise]);
+		});
+
+		return ret;
+	};
+}
+
+function checkForExistingKeys(options) {
+	if (zoneStorage.getItem(options.storageKeys.currentProperty) ||
+		zoneStorage.getItem(options.storageKeys.currentPropertyPromise) ||
+		zoneStorage.getItem(options.storageKeys.savingProperty)) {
+		console.warn('can-define-connected-singleton: Removing existing values from zone storage. You are likely configuring a singleton twice.');
+		zoneStorage.removeItem(options.storageKeys.currentProperty);
+		zoneStorage.removeItem(options.storageKeys.currentPropertyPromise);
+		zoneStorage.removeItem(options.storageKeys.savingProperty);
+	}
+}
+
+// get stored current & promise values, initializing them if they're not yet set
+function getCurrentAndPromise(Ctor, options) {
+	var current = zoneStorage.getItem(options.storageKeys.currentProperty);
+	var promise = zoneStorage.getItem(options.storageKeys.currentPropertyPromise);
+
+	if (promise == null) {
+		promise = Ctor[options.fetchMethodName]();
+		zoneStorage.setItem(options.storageKeys.currentPropertyPromise, promise);
+		Ctor.dispatch(options.currentPropertyPromiseName, [promise]);
+
+		promise.then(function (value) {
+			zoneStorage.setItem(options.storageKeys.currentProperty, value);
+			Ctor.dispatch(options.currentPropertyName, [value]);
+		})
+		.catch(function () {
+			zoneStorage.setItem(options.storageKeys.currentProperty, null);
+			Ctor.dispatch(options.currentPropertyName, [null]);
+		});
+	}
+
+	return {
+		current: current,
+		promise: promise
+	};
+}
+
+
+function makeSingleton(Ctor, input_options){
+	var helpURL = 'https://canjs.com/doc/can-define-connected-singleton';
+
+	if(!isDefineMapConstructor(Ctor)) {
+		throw new Error('The singleton decorator/mixin can only be used for DefineMaps: ' + helpURL);
+	}
+
+	var savingPropertyName = input_options.savingPropertyName;
+	var currentPropertyName = input_options.currentPropertyName;
+	var currentPropertyPromiseName = currentPropertyName + 'Promise';
+	var currentPropertyKey = input_options.storagePrefix + '-' + Ctor.name + '-' + currentPropertyName;
+	var storageKeys = {
+		savingProperty: input_options.storagePrefix + '-' + Ctor.name + '-' + savingPropertyName,
+		currentProperty: currentPropertyKey,
+		currentPropertyPromise: currentPropertyKey + '-promise'
+	};
+	var options = Object.assign({}, input_options, {
+		currentPropertyPromiseName, storageKeys
+	});
+
+	checkForExistingKeys(options);
+
+	Object.defineProperty(Ctor, options.currentPropertyPromiseName, {
+		get: function () {
+			ObservationRecorder.add(Ctor, options.currentPropertyPromiseName);
+			return getCurrentAndPromise(Ctor, options).promise;
+		}
+	});
+
+	Object.defineProperty(Ctor, options.currentPropertyName, {
+		get: function () {
+			ObservationRecorder.add(Ctor, options.currentPropertyName);
+			return getCurrentAndPromise(Ctor, options).current;
+		}
+	});
+
+	wrapCreateMethod(Ctor, options);
+	wrapDestroyMethod(Ctor, options);
+
+	return Ctor;
+}
+
+function singleton(Obj) {
+	var opts = helpers.assign({}, defaults);
+
+	// @singleton
+	if(isDefineMapConstructor(Obj)) {
+		return makeSingleton(Obj, opts);
+	}
+
+	// @singleton(options)
+	return function(Ctor) {
+		return makeSingleton(Ctor, helpers.assign(opts, Obj));
+	};
+}
+
+module.exports = singleton;
